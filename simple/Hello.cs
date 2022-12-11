45bb30161b5ba080d23158a98938d7deae8c003f
Kojiro ONO <ono.kojiro@gmail.com>
2022-12-11 21:23:21 +0900

    add Hello World

---
+++ simple/Hello.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MyNameSpace
+{
+    internal class HelloWorld
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello, world!");
+        }
+    }
+}
