352b4840605b5304eec71954e105e78f370bfaf4
Kojiro ONO <ono.kojiro@gmail.com>
2022-12-11 23:06:05 +0900

    add xml sample

---
+++ xml/Hello.cs
@@ -0,0 +1,80 @@
+using System;
+
+using System.IO;
+using System.Text;
+
+using Gnu.Getopt;
+
+using System.Xml;
+
+namespace MyNameSpace
+{
+    internal class HelloWorld
+    {
+        static void Main(String[] args)
+        {
+            int ret = 0;
+
+            int c;
+            int index;
+            String output = "";
+
+            LongOpt[] longopts = new LongOpt[] {
+                new LongOpt("help", Argument.No, null, 'h'),
+                new LongOpt("version", Argument.No, null, 'v'),
+                new LongOpt("output", Argument.Required, null, 'o'),
+            };
+
+            Getopt g = new Getopt("myprogram", args, "hvo:", longopts);
+
+            while ((c = g.getopt()) != -1)
+            {
+                switch(c)
+                {
+                    case 'h':
+                        break;
+                    case 'v':
+                        break;
+                    case 'o':
+                        output = g.Optarg;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            if(output == ""){
+              Console.WriteLine("ERROR : no output option");
+              ret++;
+            }
+
+            if(ret != 0){
+                Environment.Exit(ret);
+            }
+
+            Console.WriteLine("Hello, world!");
+
+            // open StreamWriter without BOM
+            Encoding enc = new UTF8Encoding(false);
+            StreamWriter writer = new StreamWriter(output, false, enc);
+
+            writer.WriteLine("日本語");
+
+            for(index = g.Optind; index < args.Length; index++){
+                Console.WriteLine("INFO : read {0}", args[index]);
+                StreamReader reader = new StreamReader(args[index]);
+
+                string lines = reader.ReadToEnd();
+                Console.WriteLine(lines);
+                reader.Close();
+
+
+                XmlDocument doc = new XmlDocument();
+                doc.LoadXml(lines);
+                doc.Save(writer);
+            }
+
+            writer.Close();
+        }
+    }
+}
---
+++ xml/build.bat
@@ -0,0 +1,105 @@
+@ECHO OFF
+@SETLOCAL ENABLEDELAYEDEXPANSION
+
+SET PATH=%SYSTEMROOT%\System32
+
+SET PATH=C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin;%PATH%
+
+SET PROJECT=myproject.csproj
+SET TARGETFRAMEWORKVERSION=v4.7.2
+SET CONFIGURATION=Debug
+
+IF "x%1" == "x" (
+	CALL :ALL
+	REM disable echo because subroutine might enable echo
+	@ECHO OFF
+	IF NOT !ERRORLEVEL! == 0 (
+		ECHO ERROR : ALL returned !ERRORLEVEL!
+		EXIT /B !ERRORLEVEL!
+	)
+) else (
+	FOR %%i IN (%*) DO (
+		CALL :_CHECK_LABEL %%i
+		IF !ERRORLEVEL! == 0 (
+			CALL :%%i
+			REM disable echo because subroutine might enable echo
+			@ECHO OFF
+
+			IF NOT !ERRORLEVEL! == 0 (
+				ECHO ERROR : %%i returned !ERRORLEVEL!
+				EXIT /B !ERRORLEVEL!
+			)
+		) ELSE (
+			ECHO ERROR : no such label, "%%i"
+			EXIT /B 1
+		)
+		
+	)
+)
+
+ENDLOCAL & SET RETVAL=%ERRORLEVEL%
+@EXIT /B !RETVAL!
+
+REM ===============================
+REM === All
+REM ===============================
+:ALL
+CALL :BUILD
+GOTO :EOF
+
+REM ===============================
+REM === Version
+REM ===============================
+:VERSION
+msbuild.exe -version
+GOTO :EOF
+
+REM ===============================
+REM === Restore
+REM ===============================
+:RESTORE
+msbuild -t:restore -p:RestorePackagesConfig=true !PROJECT!
+GOTO :EOF
+
+REM ===============================
+REM === Build
+REM ===============================
+:BUILD
+MSBuild.exe -t:Build !PROJECT! ^
+    -property:Configuration=!CONFIGURATION!;TargetFrameworkVersion=!TARGETFRAMEWORKVERSION!
+GOTO :EOF
+
+REM ===============================
+REM === Clean
+REM ===============================
+:CLEAN
+MSBuild.exe -t:Clean !PROJECT!
+GOTO :EOF
+
+REM ===============================
+REM === Maintainer Clean
+REM ===============================
+:MCLEAN
+CALL :CLEAN
+RMDIR /S /Q obj bin packages
+GOTO :EOF
+
+REM ===============================
+REM === Test
+REM ===============================
+:TEST
+Bin\Debug\example.exe -o output.txt input.xml
+GOTO :EOF
+
+REM ===============================
+REM === _CHECK_LABEL
+REM ===============================
+:_CHECK_LABEL
+FINDSTR /I /R /C:"^[ ]*:%1\>" "%~f0" >NUL 2>NUL
+GOTO :EOF
+
+REM ===============================
+REM === END
+REM ===============================
+:END
+GOTO :EOF
